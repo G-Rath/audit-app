17c2e3e78b109a17490e3006274acb69fbee93e9
Gareth Jones <jones258@gmail.com>
2020-07-26 12:19:39 +1200

    chore: break `buildAdvisory` out into its own exported function

--- test/src/formatReport.spec.ts
+++ test/src/formatReport.spec.ts
@@ -1,7 +1,7 @@
 import stripAnsi from 'strip-ansi';
 import { SupportedReportFormat, formatReport } from '../../src/formatReport';
 import { AuditReport } from '../../src/generateReport';
-import { Advisory } from '../../src/types';
+import { buildAdvisory } from '../buildAdvisory';
 
 const emptyReport: AuditReport = {
   statistics: {},
@@ -10,41 +10,6 @@ const emptyReport: AuditReport = {
   ignored: [],
   missing: []
 };
-
-const buildAdvisory = (advisory: Partial<Advisory>): Advisory => ({
-  findings: [
-    {
-      version: '10.1.0',
-      paths: ['@commitlint/cli>meow>yargs-parser']
-    },
-    {
-      version: '9.0.2',
-      paths: [
-        'semantic-release>@semantic-release/npm>npm>libnpx>yargs>yargs-parser'
-      ]
-    }
-  ],
-  id: 1500,
-  created: '2020-03-26T19:21:50.174Z',
-  updated: '2020-05-01T01:05:15.020Z',
-  deleted: null,
-  title: 'Prototype Pollution',
-  found_by: { link: '', name: 'Snyk Security Team', email: '' },
-  reported_by: { link: '', name: 'Snyk Security Team', email: '' },
-  module_name: 'yargs-parser',
-  cves: [],
-  vulnerable_versions: '',
-  patched_versions: '',
-  overview: '',
-  recommendation: '',
-  references: '',
-  access: 'public',
-  severity: 'low',
-  cwe: 'CWE-471',
-  metadata: { module_type: '', exploitability: 1, affected_components: '' },
-  url: 'https://npmjs.com/advisories/1500',
-  ...advisory
-});
 
 const formatReportAndStripAnsi = (
   format: SupportedReportFormat,
--- test/src/generateReport.spec.ts
+++ test/src/generateReport.spec.ts
@@ -1,41 +1,6 @@
 import { AuditResults } from '../../src/audit';
 import { AuditReport, generateReport } from '../../src/generateReport';
-import { Advisory } from '../../src/types';
-
-const buildAdvisory = (advisory: Partial<Advisory>): Advisory => ({
-  findings: [
-    {
-      version: '10.1.0',
-      paths: ['@commitlint/cli>meow>yargs-parser']
-    },
-    {
-      version: '9.0.2',
-      paths: [
-        'semantic-release>@semantic-release/npm>npm>libnpx>yargs>yargs-parser'
-      ]
-    }
-  ],
-  id: 1500,
-  created: '2020-03-26T19:21:50.174Z',
-  updated: '2020-05-01T01:05:15.020Z',
-  deleted: null,
-  title: 'Prototype Pollution',
-  found_by: { link: '', name: 'Snyk Security Team', email: '' },
-  reported_by: { link: '', name: 'Snyk Security Team', email: '' },
-  module_name: 'yargs-parser',
-  cves: [],
-  vulnerable_versions: '',
-  patched_versions: '',
-  overview: '',
-  recommendation: '',
-  references: '',
-  access: 'public',
-  severity: 'low',
-  cwe: 'CWE-471',
-  metadata: { module_type: '', exploitability: 1, affected_components: '' },
-  url: 'https://npmjs.com/advisories/1500',
-  ...advisory
-});
+import { buildAdvisory } from '../buildAdvisory';
 
 describe('generateReport', () => {
   const results: AuditResults = {
